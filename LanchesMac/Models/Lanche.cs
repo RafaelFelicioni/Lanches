61e7d713f51d30be8ff80cd4ab9050ea205bfbcd
RafaelFelicioni <rafaelalmeida1995@outlook.com>
2020-11-05 01:39:59 -0300

    primeiro commit

---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
+{
+    [Table("Lanches")]
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+
+        [Required]
+        [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength = 10)]
+        public string Nome { get; set; }
+
+        [Required]
+        [Display(Name = "Descrição do Lanche")]
+        [MinLength(20)]
+        [MaxLength(200)]
+        public string DescricaoCurta { get; set; }
+
+        [Required]
+        [Display(Name = "Descrição detalhada do Lanche")]
+        [MinLength(20)]
+        [MaxLength(200)]
+        public string DescricaoDetalhada { get; set; }
+
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200)]
+        public string ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho Imagem Miniatura")]
+        [StringLength(200)]
+        public string ImagemThumbnailUrl { get; set; }
+
+        [Display(Name = "Preferido?")]
+        public bool IsLanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
+        public bool EmEstoque { get; set; }
+
+        [Display(Name = "Categoria")]
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
---
+++ LanchesMac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.ViewModels
+{
+    public class LoginViewModel
+    { 
+
+        [Required]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        [StringLength(
+            100,
+            ErrorMessage = "The {0} must be at least {2} characters long.",
+             MinimumLength = 6)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
